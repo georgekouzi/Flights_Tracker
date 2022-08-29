ed2874ac37771f16771556d73f7e2c63c592071e
george <georgekouzi@gmail.com>
2022-08-29 12:37:16 +0300

    add func getCity& getTime on datafetch

--- DataCollection/DataFetch.js
+++ DataCollection/DataFetch.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const axios = require('axios');
 
 
@@ -53,27 +54,74 @@ function getPeriod(){
     var year = date_ob.getFullYear();
     
     var date = year + "-" + month + "-" + day;
-    // console.log(date);
+   
+    const Hholyday = ["Rosh Hashana","Sukkot","Yom Kippur","Shmini Atzeret","Lag BaOmer","Simchat Torah","Chanukah","Pesach","Shavuot","Purim",]
     return new Promise ( res => { 
-        axios.get('https://www.hebcal.com/converter?cfg=json&start=2021-12-29&end=2022-01-04&g2h=1')
+        axios.get(`https://www.hebcal.com/converter?cfg=json&date=${date}&g2h=1`)
          .then(response => {
 
-            if(response.data.hdates[date]){
-                res('holiday')
-            }else {
-                if(date_ob.getMonth() >= 6  && date_ob.getMonth() <= 8) {
-                    res('summer')
-                }else{
-                    res('normal day')
-                }
-
-            }
+            var arr2 = response.data.events
             
-        })
+            for (let i = 0; i < arr2.length; i++) {
+                const element = arr2[i];
+                Hholyday.forEach(element2 => {
+                    if(element.includes(element2)){
+                        res('holiday')
+                        return ;
+                    }else {
+                        if(date_ob.getMonth() >= 6  && date_ob.getMonth() <= 8) {
+                            res('summer')
+                            return;
+                        }else{
+                                res('normal day')
+                                return;
+                        }
+                    }
+                });
+                
+            }
+                     
+            });
+               
+            });
+            
+            
+        
+}
+
+
+function getCity(city){
+    if(city){
+    return new Promise ( res => { 
+    axios.get(`https://airlabs.co/api/v9/cities?city_code=${city}&_fields=name,lat,lng&api_key=c2515044-e704-4c46-a60f-03eb10588f30`).then(response => {
+        // console.log(response)
+        res(response.data.response)            
+    }).catch(error => {
+        console.log(error);
+});
     })
 }
-// getPeriod()
-// getWeather('');
+}
+
+// console.log( getTime("2021-07-22 22:53"))
+function getTime(timeStamp){
+    var time = timeStamp.trim().split(/\s+/)[1].split(":");
+    var hours = parseInt(time[0])*60
+    var min = parseInt(time[1])
+    
+    var today = new Date();
+    var TimeInMin = ((parseInt(today.getHours()))*60 + parseInt(today.getMinutes()));
+    
+    if(Math.abs((hours+min)- TimeInMin) <= 15){
+        return true
+    }else return false ;
+
+}
+
+
+// getPeriod().then(res => {console.log(res)})
+module.exports.getTime = getTime;
+module.exports.getCity = getCity;
 module.exports.getFlights = getFlights;
 module.exports.getWeather = getWeather;
 module.exports.flightINFO= flightINFO;
